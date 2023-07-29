0ee86f8800c330bdac32fa539d603ed295c09826
numanrki <bhatti3742@gmail.com>
2023-07-29 20:27:04 +0500

    Trending GIF Added

---
+++ psf-core/psf-trending-nogif.php
@@ -0,0 +1,49 @@
+<?php
+//PSF Trending Posts Show
+// Shortcode callback function
+add_shortcode('psf-trending', 'psf_trending_posts');
+function psf_trending_posts($atts) {
+    // Extract attributes from the shortcode
+    $atts = shortcode_atts(array(
+        'show' => '',    // Comma-separated category slugs
+        'hide' => '',    // Comma-separated category slugs to hide
+        'posts' => 5,    // Number of posts to display
+    ), $atts);
+
+    // Get the trending posts based on the shortcode attributes
+    $trending_posts = psf_get_trending_posts($atts['show'], $atts['hide'], $atts['posts']);
+
+    // Start building the output
+    $output = '<ul class="psf-trending-posts">';
+
+    foreach ($trending_posts as $post) {
+        $output .= '<li><a href="' . get_permalink($post->ID) . '">' . get_the_title($post->ID) . '</a></li>';
+    }
+
+    $output .= '</ul>';
+
+    return $output;
+}
+function psf_get_trending_posts($show_categories, $hide_categories, $posts) {
+    $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'posts_per_page' => $posts,
+        'meta_key' => 'post_views_count', // Replace 'post_views_count' with your post view count meta key
+        'orderby' => 'meta_value_num',
+        'order' => 'DESC'
+    );
+
+    // If 'show' attribute is provided and not 'all', include specified categories
+    if ($show_categories && $show_categories !== 'all') {
+        $args['category_name'] = $show_categories;
+    }
+
+    // If 'hide' attribute is provided, exclude specified categories
+    if ($hide_categories) {
+        $args['category__not_in'] = explode(',', $hide_categories);
+    }
+
+    $trending_query = new WP_Query($args);
+    return $trending_query->posts;
+}
--- psf-core/psf-updated-gif.php
+++ psf-core/psf-updated-gif.php
@@ -1,9 +1,4 @@
 <?php
-// Assuming that psf-no-gif.php is located in the main plugin directory
-// Use '../' to navigate one level up to the main plugin directory
-// Then specify the path to the gif from there
-$new_gif_image_url = plugin_dir_url( __FILE__ ) . 'assets/gifs/new.gif';
-
 // Last updated Posts Filters With GIF
 function psf_last_updated_posts_shortcode($atts) {
     $atts = shortcode_atts( array(
@@ -20,27 +15,27 @@ function psf_last_updated_posts_shortcode($atts) {
     );
 
     // Include specific categories
-    if ( ! empty( $atts['show'] ) && $atts['show'] !== 'all' ) {
-        $category_slugs = explode( ',', $atts['show'] );
-        $category_ids = array_map( 'get_category_by_slug', $category_slugs );
-        $args['category__in'] = wp_list_pluck( $category_ids, 'term_id' );
+    if (!empty($atts['show']) && $atts['show'] !== 'all') {
+        $category_slugs = explode(',', $atts['show']);
+        $category_ids = array_map('get_category_by_slug', $category_slugs);
+        $args['category__in'] = wp_list_pluck($category_ids, 'term_id');
     }
 
     // Exclude specific categories
-    if ( ! empty( $atts['hide'] ) ) {
-        $category_slugs = explode( ',', $atts['hide'] );
-        $category_ids = array_map( 'get_category_by_slug', $category_slugs );
-        $args['category__not_in'] = wp_list_pluck( $category_ids, 'term_id' );
+    if (!empty($atts['hide'])) {
+        $category_slugs = explode(',', $atts['hide']);
+        $category_ids = array_map('get_category_by_slug', $category_slugs);
+        $args['category__not_in'] = wp_list_pluck($category_ids, 'term_id');
     }
 
     $last_updated_posts = new WP_Query($args);
 
     if ($last_updated_posts->have_posts()) {
-        $output = '<ul class="psf-last-updated-posts">'; // Add the custom class here
+        $output = '<ul class="psf-last-updated-posts">';
 
         while ($last_updated_posts->have_posts()) {
             $last_updated_posts->the_post();
-            $output .= '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a><img src="' . esc_url( $new_gif_image_url ) . '" alt="New" class="psf-new-gif" width="24" height="24" /></li>';
+            $output .= '<li><a href="' . get_permalink() . '">' . get_the_title() . '<img src="' . plugin_dir_url(__FILE__) . '../assets/gifs/new.gif" alt="New" class="psf-new-gif" width="32" height="32" /></a></li>';
         }
 
         $output .= '</ul>';
